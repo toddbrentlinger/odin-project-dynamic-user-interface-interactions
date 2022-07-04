d0fc362c24495d9fbc463f0fea72d7dcd7646e2f
toddbrentlinger <toddbrentlinger@gmail.com>
2022-07-03 18:39:21 -0700

    Improve baseComponent class
    Adds props object as constructor argument.
    Moves logic from beginning of render that either creates the element or
    empties it of it's children elements into separate method.

--- src/baseComponent.js
+++ src/baseComponent.js
@@ -2,18 +2,46 @@
 import { createElement } from './utilities.js';
 
 export default class BaseComponent {
-    constructor() {
+    constructor(props) {
         this._element = null;
+        this._props = props;
     }
+
+    // Getters/Setters
 
     get element() {
         return this._element;
     }
 
-    render() {
-        if (!this._element) {
-            this._element = document.createElement('div');
+    get props() {
+        return this._props;
+    }
+
+    set props(newProps) {
+        this._props = newProps;
+        this.render();
+    }
+
+    // Methods
+
+    emptyElement() {
+        if (!this._element) return;
+
+        while (this._element.firstChild) {
+            this._element.removeChild(this._element.firstChild);
         }
+    }
+
+    initializeRender(baseElement = document.createElement('div')) {
+        if (!this._element) {
+            this._element = baseElement;
+        } else {
+            this.emptyElement();
+        }
+    }
+
+    render() {
+        this.initializeRender();
 
         this._element.appendChild(
             createElement('h1', {}, 'Base Component!')
